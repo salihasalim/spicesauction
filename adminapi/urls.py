f4ae9193e32cd543d74f1b53ab2ee7eb50e8cd89
sidharths <sidharthsuresh599@gmail.com>
2024-03-26 16:56:42 +0530

    commit

--- adminapi/urls.py
+++ adminapi/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from adminapi import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -12,4 +14,4 @@ urlpatterns=[
 
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
