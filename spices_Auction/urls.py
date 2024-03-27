0d9f138b88dc7727dac92a39d0d67d2b8e3b9f0d
sidharths <sidharthsuresh599@gmail.com>
2024-03-27 08:26:42 +0530

    design

--- spices_Auction/urls.py
+++ spices_Auction/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('adminapi/',include("adminapi.urls")),
-    path('userapi/',include("userapi.urls"))
+    path('',include("userapi.urls"))
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
